c8a45d7b52701f2a94689eaea3d92c217c448f10
Stefan <stefan.valchinov@i.tescobank.com>
2016-06-06 13:42:57 +0100

    changed few names

--- src/main/java/TicketMachine.java
+++ src/main/java/TicketMachine.java
@@ -5,19 +5,19 @@ public class TicketMachine {
     final static int weeklyTicketPrice = 7;
     final static int monthlyTicketPrice = 25;
 
-    public int solution(int[] monthDays) {
+    public int solution(int[] days) {
 
         int total = 0;
         int singleTicketTotal = 0;
-        int firstNoTicketDay = monthDays[0];
+        int firstNoTicketDay = days[0];
         int sevenDayPurchaseDate = -1;
 
-        for (int i = 0; i < monthDays.length; i++) {
-            if (monthDays[i] - sevenDayPurchaseDate <= 7 && sevenDayPurchaseDate > 0) {
-                firstNoTicketDay = (i + 1 < monthDays.length) ? monthDays[i + 1] : monthDays[i];
+        for (int i = 0; i < days.length; i++) {
+            if (days[i] - sevenDayPurchaseDate <= 7 && sevenDayPurchaseDate > 0) {
+                firstNoTicketDay = (i + 1 < days.length) ? days[i + 1] : days[i];
             } else {
                 singleTicketTotal += singleTicketPrice;
-                if (singleTicketTotal + singleTicketPrice > 7 && monthDays[i] - firstNoTicketDay <= 7) {
+                if (singleTicketTotal + singleTicketPrice > weeklyTicketPrice && days[i] - firstNoTicketDay <= 7) {
                     total += weeklyTicketPrice;
                     singleTicketTotal = 0;
                     sevenDayPurchaseDate = firstNoTicketDay;
